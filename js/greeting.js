43fcca188dd7272fe4c5852fa538e35571e0d7b8
mnhycr <one.914.mind@gmail.com>
2021-01-14 18:02:53 +0300

    v2

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = '';
+// const name = ' ';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
@@ -15,11 +15,11 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = gree1;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById('greetings').innerText = gree2;
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+  document.getElementById('greetings').innerText = gree3;
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById('greetings').innerText = gree4;
 }
