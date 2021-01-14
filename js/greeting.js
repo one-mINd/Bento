0a941150a6d76f7c6e021c13d78e278f1d5cdd29
mnhycr <one.914.mind@gmail.com>
2021-01-14 18:24:11 +0300

    v3

--- js/greeting.js
+++ js/greeting.js
@@ -9,9 +9,9 @@ const hour = today.getHours();
 const gree1 = 'Go to Sleep!  ';
 const gree2 = 'Good morning!  ';
 const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const gree4 = 'Good evening  ';
+const gree5 = 'Good evening  ';
+const gree6 = 'Good evening  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
